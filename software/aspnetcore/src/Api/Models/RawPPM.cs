b542ab7bf29346560d3de02bcb96c61e5eb3336d
MrSzymonello <szymonszyszko@wp.pl>
2018-04-10 22:18:42 +0200

    Remove code
    The code has been already moved to separate repositories

--- software/aspnetcore/src/Api/Models/RawPPM.cs
+++
@@ -1,9 +0,0 @@
-namespace Api.Models
-{
-    public class RawPPM
-    {
-        public string Base64samples { get; set; }
-        public double TakenAt { get; set; }
-        public double SampleRate { get; set; }
-    }
-}
--- software/c/base64.h
+++
@@ -1,6 +0,0 @@
-#ifndef __BASE64_H
-#define __BASE64_H
-
-void base64_encode(unsigned char const*, unsigned char*);
-
-#endif
